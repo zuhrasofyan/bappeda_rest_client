4c72b3a67809cfcefc6954ad165706acf1532a23
zuhrasofyan <zuhra.sofyan@rwth-aachen.de>
2017-03-17 11:55:07 +0700

    Fixed main page display

--- src/app/components/title/title.js
+++ src/app/components/title/title.js
@@ -1,5 +1,12 @@
 angular
   .module('app')
   .component('fountainTitle', {
-    templateUrl: 'app/components/title/title.html'
+    templateUrl: 'app/components/title/title.html',
+    controller: titleController,
+    controllerAs: 'vm'
   });
+
+function titleController($rootScope) {
+	var vm = this;
+	vm.isAuthenticated = $rootScope.isAuthenticated;
+}
