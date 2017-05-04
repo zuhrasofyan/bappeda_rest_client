4e00220e5b4b16cbdab6b0cca95616c8d0976e44
zuhrasofyan <zuhrasofyan@gmail.com>
2017-05-04 13:34:29 +0700

    .

--- src/app/services/AvatarService.js
+++ src/app/services/AvatarService.js
@@ -8,21 +8,10 @@ function AvatarService($http, $state) {
   function getAvatarImage(id) {
     var avatarUrl = 'http://localhost:1337/user/avatar/'+id;
     return avatarUrl;
-
-    // $http.get('http://localhost:1337/user/avatar/'+id)
-    // .then(function(result){
-    //   if (result.status !== 200) {
-    //     console.log(avatarUrl);
-    //     return avatarUrl;
-    //   } else {
-    //     return 'haha';
-    //   }
-    // })
     
   }
 
   function uploadAvatar(files, id) {
-    console.log('hei');
     var formData = new FormData();
     angular.forEach(files, function(obj){
       if (!obj.isRemote) {
@@ -36,7 +25,6 @@ function AvatarService($http, $state) {
     .then(function(result){
       if (result.status === 200) {
         alert('Berhasil di unggah');
-        console.log(result);
         $state.reload();
       } else {
         alert('terjadi kesalahan. coba lagi');
