7fbfd06ce686dc039301e794505a522acbdaa931
zuhrasofyan <zuhrasofyan@gmail.com>
2017-05-30 11:34:59 +0700

    Implement summary of persentasi last year RAD capaian bar-chart

--- src/app/pages/homeDashboard/homeDashboard.js
+++ src/app/pages/homeDashboard/homeDashboard.js
@@ -4,12 +4,76 @@ angular
     templateUrl: 'app/pages/homeDashboard/homeDashboard.html',
     controller: homeDashboardController,
     controllerAs: 'vm'
-  });
+  })
 
-function homeDashboardController(UserService, $http) {
+function homeDashboardController(UserService, $http, RadService) {
   vm = this;
 
-  //general
+  vm.options = {
+    responsive: true,
+    scales:{
+      xAxes:[{
+        // ticks: {
+        //   callback: function(value) {
+        //     return value.substr(0,30);
+        //   }
+        // }
+      }],
+      yAxes:[{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+    tooltips: {
+      callbacks: {
+        beforeTitle: function (tooltipItems, data){
+          return 'Kategori:';
+        },
+        label: function(tooltipItems, data) {
+          return tooltipItems.yLabel + ' %';
+        }
+      }
+    }
+  }
+
+  //set initial years
+  var currentTime = new Date();
+  var curYear = currentTime.getFullYear();
+  var lastYear = curYear -1;
+  vm.curYear = curYear;
+  vm.lastYear = lastYear;
+  vm.tahun = '';
+
+  // Initial data retrieved from REST API
+  // vm.persentasi = [];
+  // vm.masalah = [];
+
+  vm.jenisMasalah = [];
+  vm.masalahPersentasi = [];
+  vm.avgMasalahPersentasi = [];
+  RadService.getRadDataTahunan(vm.lastYear).then(function(d){
+    vm.dataTahunan = d.data;
+    for (i=0; i< d.data.length; i++){
+      // push each kategori into jenis masalah
+      vm.jenisMasalah[i] = d.data[i].kategori;
+      var arr = [];
+      var x = 0;
+      for (j=0; j< d.data[i].renaksi.length; j++) {
+        arr.push(d.data[i].renaksi[j].persentasi_capaian);
+        x= x+d.data[i].renaksi[j].persentasi_capaian;
+      }
+      // push to array:
+      // each persentasi into multidimensional array
+      vm.masalahPersentasi.push(arr);
+      // sum an average of each kategory percentage
+      vm.avgMasalahPersentasi.push(Math.floor(x/(d.data[i].renaksi.length)));
+    }
+    // angular.forEach(d.data[0].renaksi, function(value){
+    //   vm.persentasi.push(value.persentasi_capaian);
+    //   vm.masalah.push(value.masalah);
+    // })
+  });
   
 
   //for tab user 
